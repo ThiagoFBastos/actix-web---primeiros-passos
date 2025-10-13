3a75b4f6a2506cf1d000cf0b1d4fb4b27a629430
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-10-13 20:57:17 -0300

    refactor: deixando os derive em um único annotation

--- src/entidades/endereco.rs
+++ src/entidades/endereco.rs
@@ -1,11 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
-#[derive(Deserialize)]
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
+
 pub struct Endereco {
     pub cep: String,
     pub endereco: String,
-    pub complemento: Option<String>,
+    pub complemento: Option<String>
 }
--- src/entidades/pessoa.rs
+++ src/entidades/pessoa.rs
@@ -1,13 +1,10 @@
 use serde::{Deserialize, Serialize};
 use crate::entidades::endereco::{Endereco};
 
-#[derive(Serialize)]
-#[derive(Deserialize)]
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Pessoa {
     pub nome: String,
     pub idade: u32,
     pub endereco: Endereco,
-    pub cpf: String,
+    pub cpf: String
 }
